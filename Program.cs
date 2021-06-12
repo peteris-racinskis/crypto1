4aa5d743a530689303878c199e35f37deab38558
Your Name <peterisracinskis@gmail.com>
2021-06-12 20:44:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace crypto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i;
+            i = 5;
+            i = 7;
+            Console.WriteLine("This is after the breakpoint"+i.ToString());
+        }
+    }
+}
